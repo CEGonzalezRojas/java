6216b9e7954d75a393f58e9240bb2f336f45c773
cegonzal <cegonzal@CEGRnote>
2013-03-16 01:28:09 -0400

    New Project

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author cegonzal
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int alpha = 1;
+        System.out.print(alpha);
+    }
+}
